f965713430220e7122dbffb3e9cdb937e4d7c37b
Sérgio Manoel Torres <sergio.torres@interprocess.com.br>
2025-05-24 16:24:46 -0300

    Criação da camada App, com as dtos para intermediar entre api e services

---
+++ SimulacaoEmprestimo/App/DTO/ValorEntradaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DTO
+{
+    public class ValorEntradaDTO
+    {
+        public decimal LoanAmount { get; set; }
+        public float AnnualInterestRate { get; set; }
+        public int NumberOfMoths { get; set; }
+    }
+}
