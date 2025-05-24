ebb56ce17575a2fdab742bffa37845ae94bdf011
Sérgio Manoel Torres <sergio.torres@interprocess.com.br>
2025-05-24 16:25:24 -0300

    Criação das entidades Parcela e Proposta

---
+++ SimulacaoEmprestimo/Domain/Models/Parcela.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Parcela
+    {
+        public int Id { get; set; }
+        public int PropostaId { get; set; }
+        public int Month { get; set; }
+        public decimal Principal { get; set; }
+        public decimal Interest { get; set; }
+        public decimal Balance { get; set; }
+        public Proposta Proposta { get; set; }
+    }
+}
---
+++ SimulacaoEmprestimo/Domain/Models/Proposta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Proposta
+    {
+        public int Id { get; set; }
+        public decimal MonthlyPayment { get; set; }
+        public decimal TotalInterest { get; set; }
+        public decimal TotalPayment { get; set; }
+        public ICollection<Parcela> Parcelas { get; set; }
+    }
+}
